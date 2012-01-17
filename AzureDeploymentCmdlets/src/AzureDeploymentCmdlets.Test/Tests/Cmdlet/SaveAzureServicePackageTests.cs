4b09e610ae7ec98fb5e8a2fff69c834353df3404
Aaron Stannard <aaron@aaronstannard.com>
2012-01-17 00:03:17 -0800

    Finished moving Save-AzureServicePackage into unit test coverage. All tests currently pass.

--- AzureDeploymentCmdlets/src/AzureDeploymentCmdlets.Test/Tests/Cmdlet/SaveAzureServicePackageTests.cs
+++ AzureDeploymentCmdlets/src/AzureDeploymentCmdlets.Test/Tests/Cmdlet/SaveAzureServicePackageTests.cs
@@ -93,5 +93,136 @@ namespace AzureDeploymentCmdlets.Test.Tests.Cmdlet
 
             }
         }
+
+        /// <summary>
+        /// Test a basic packaging scenario for an Azure Service Package with a single web role.
+        /// </summary>
+        [TestMethod]
+        public void SavePackageWithOneNodeWorkerRoleTest()
+        {
+            //Create a temp directory for monitoring and cleaning up the output of our test
+            using (FileSystemHelper files = new FileSystemHelper(this) { EnableMonitoring = true })
+            {
+                //Create a new service that we're going to pack locally
+                string serviceName = "TEST_SERVICE_NAME";
+                NewAzureServiceCommand newService = new NewAzureServiceCommand();
+                newService.NewAzureServiceProcess(files.RootPath, serviceName);
+                string servicePath = files.CreateDirectory(serviceName);
+
+                //Add a Node web role to the solution
+                string roleName = "TEST_WORKER_ROLE";
+                int instanceCount = 2;
+                AddAzureNodeWorkerRoleCommand addAzureNodeWorkerRole = new AddAzureNodeWorkerRoleCommand();
+                addAzureNodeWorkerRole.AddAzureNodeWorkerRoleProcess(roleName, instanceCount, servicePath);
+
+                //Run our packaging command
+                SaveAzureServicePackageCommand saveServicePackage = new SaveAzureServicePackageCommand();
+                saveServicePackage.CreatePackage(servicePath);
+
+                //Assert that the service structure is as expected
+                AzureAssert.ScaffoldingExists(Path.Combine(files.RootPath, serviceName, roleName), Path.Combine(Resources.NodeScaffolding, Resources.WorkerRole));
+
+                // Verify the generated files
+                files.AssertFiles(new Dictionary<string, Action<string>>()
+                {
+                    {
+                        serviceName + @"\deploymentSettings.json",
+                        null
+                    },
+                    {
+                        serviceName + @"\ServiceDefinition.csdef",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\ServiceConfiguration.Cloud.cscfg",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\ServiceConfiguration.Local.cscfg",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\cloud_package.cspkg",
+                        p =>
+                        {
+                            using (Package package = Package.Open(p))
+                            {
+                                Assert.AreEqual(6, package.GetParts().Count());
+                            }
+                        }
+                    }
+                });
+
+            }
+        }
+
+        /// <summary>
+        /// Test a basic packaging scenario for an Azure Service Package with a single web role.
+        /// </summary>
+        [TestMethod]
+        public void SavePackageWithMultipleRolesTest()
+        {
+            //Create a temp directory for monitoring and cleaning up the output of our test
+            using (FileSystemHelper files = new FileSystemHelper(this) { EnableMonitoring = true })
+            {
+                //Create a new service that we're going to pack locally
+                string serviceName = "TEST_SERVICE_NAME";
+                NewAzureServiceCommand newService = new NewAzureServiceCommand();
+                newService.NewAzureServiceProcess(files.RootPath, serviceName);
+                string servicePath = files.CreateDirectory(serviceName);
+
+                //Add a Node web role to the solution
+                string webRoleName = "TEST_WEB_ROLE";
+                int webRoleInstanceCount = 2;
+                AddAzureNodeWebRoleCommand addAzureNodeWebRole = new AddAzureNodeWebRoleCommand();
+                addAzureNodeWebRole.AddAzureNodeWebRoleProcess(webRoleName, webRoleInstanceCount, servicePath);
+
+                //Add a Node web role to the solution
+                string workerRoleName = "TEST_WORKER_ROLE";
+                int workerRoleInstanceCount = 2;
+                AddAzureNodeWorkerRoleCommand addAzureNodeWorkerRole = new AddAzureNodeWorkerRoleCommand();
+                addAzureNodeWorkerRole.AddAzureNodeWorkerRoleProcess(workerRoleName, workerRoleInstanceCount, servicePath);
+
+                //Run our packaging command
+                SaveAzureServicePackageCommand saveServicePackage = new SaveAzureServicePackageCommand();
+                saveServicePackage.CreatePackage(servicePath);
+
+                //Assert that the service structure is as expected
+                AzureAssert.ScaffoldingExists(Path.Combine(files.RootPath, serviceName, workerRoleName), Path.Combine(Resources.NodeScaffolding, Resources.WorkerRole));
+                AzureAssert.ScaffoldingExists(Path.Combine(files.RootPath, serviceName, webRoleName), Path.Combine(Resources.NodeScaffolding, Resources.WebRole));
+
+                // Verify the generated files
+                files.AssertFiles(new Dictionary<string, Action<string>>()
+                {
+                    {
+                        serviceName + @"\deploymentSettings.json",
+                        null
+                    },
+                    {
+                        serviceName + @"\ServiceDefinition.csdef",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\ServiceConfiguration.Cloud.cscfg",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\ServiceConfiguration.Local.cscfg",
+                        p => File.ReadAllText(p).Contains(serviceName)
+                    },
+                    {
+                        serviceName + @"\cloud_package.cspkg",
+                        p =>
+                        {
+                            using (Package package = Package.Open(p))
+                            {
+                                Assert.AreEqual(7, package.GetParts().Count());
+                            }
+                        }
+                    }
+                });
+
+            }
+        }
     }
 }
