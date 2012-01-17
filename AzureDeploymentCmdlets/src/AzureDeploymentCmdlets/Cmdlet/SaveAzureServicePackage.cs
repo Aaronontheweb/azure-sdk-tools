3ccf58bae0ee137fd4be4c17ea653a8090a614b3
Aaron Stannard <aaron@aaronstannard.com>
2012-01-16 23:16:34 -0800

    Finished first implementation of Save-AzureServicePackage Cmdlet implementation. Going to write some quick tests for it.

---
+++ AzureDeploymentCmdlets/src/AzureDeploymentCmdlets/Cmdlet/SaveAzureServicePackage.cs
@@ -0,0 +1,85 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright 2011 Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Security.Permissions;
+using System.Text;
+using AzureDeploymentCmdlets.Model;
+using AzureDeploymentCmdlets.Properties;
+using AzureDeploymentCmdlets.WAPPSCmdlet;
+
+namespace AzureDeploymentCmdlets.Cmdlet
+{
+    /// <summary>
+    /// Save a new deployment package (.cspack) for the current Windows Azure service. Overwrites the existing .cspack.
+    /// </summary>
+    [Cmdlet(VerbsData.Save, "AzureServicePackage")]
+    public class SaveAzureServicePackageCommand : ServiceManagementCmdletBase
+    {
+        private DeploymentSettings _deploymentSettings;
+        private AzureService _azureService;
+
+        /// <summary>
+        /// Initializes a new instance of the SaveAzureServicePackageCommand class.
+        /// </summary>
+        public SaveAzureServicePackageCommand(){}
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        [PermissionSet(SecurityAction.LinkDemand, Name = "FullTrust")]
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                base.ProcessRecord();
+
+                //Todo: Replace with reference to resource file
+                SafeWriteObjectWithTimestamp("Packaging application...");
+
+                CreatePackage(GetServiceRootPath());
+
+                //Todo: Replace with reference to resource file
+                SafeWriteObjectWithTimestamp("Packaging complete...");
+
+            }
+            catch(Exception ex)
+            {
+                SafeWriteError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Create a new .cspack for the Azure path hosted at the specified root path
+        /// </summary>
+        /// <param name="rootPath">Root path of the Azure service.</param>
+        [PermissionSet(SecurityAction.LinkDemand, Name = "FullTrust")]
+        internal void CreatePackage(string rootPath)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(rootPath), "rootPath cannot be null or empty.");
+            Debug.Assert(Directory.Exists(rootPath), "rootPath does not exist.");
+
+            _azureService = new AzureService(rootPath, null);
+
+            string unused = null;
+            _azureService.CreatePackage(DevEnv.Cloud, out unused, out unused);
+
+        }
+    }
+}
